5d821c0069563895cf6c4b002f9f0f221026cc76
aterbo <andy.terbovich@gmail.com>
2021-11-15 10:29:35 -0500

    Add project files.

---
+++ BlazoredModalTesting/Pages/EditFormDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazoredModalTesting.Pages
+{
+    public class EditFormDataModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+
+        [Required]
+        public DateTime? Date { get; set; }
+    }
+}
